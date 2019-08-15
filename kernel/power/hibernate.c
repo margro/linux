03c7d07baa1dc9b0584855d072f50351f080b9b6
Marcel Groothuis <margro.xbmc@gmail.com>
2019-08-15 19:08:20 +0200

    [I-pipe] Apply the ipipe-core-4.9.51-arm-4.patch from https://xenomai.org/downloads/ipipe/v4.x/arm/
    Minor modifications were needed to:
    - apply on the 4.9.189 kernel
    - fit with the Raspberry Pi kernel changes

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -290,6 +290,7 @@ static int create_image(int platform_mode)
 		goto Enable_cpus;
 
 	local_irq_disable();
+	hard_cond_local_irq_disable();
 
 	error = syscore_suspend();
 	if (error) {
@@ -455,6 +456,7 @@ static int resume_target_kernel(bool platform_mode)
 		goto Enable_cpus;
 
 	local_irq_disable();
+	hard_cond_local_irq_disable();
 
 	error = syscore_suspend();
 	if (error)
@@ -573,6 +575,7 @@ int hibernation_platform_enter(void)
 		goto Enable_cpus;
 
 	local_irq_disable();
+	hard_cond_local_irq_disable();
 	syscore_suspend();
 	if (pm_wakeup_pending()) {
 		error = -EAGAIN;
